cb21ece1db5e35dd70d2b8c8f8fdb375157128a7
Ivan Robles <irae45@gmail.com>
2017-10-10 23:35:42 -0500

    Implementamos redux-devtools-extension para poder ver las acciones ver como despachan la data y monitorear los estados de la aplicación

--- source/store.js
+++ source/store.js
@@ -1,13 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
-// import { createLogger } from 'redux-logger';
+import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducer from './reducer';
 
 const store = createStore(
   reducer,
-  applyMiddleware(
-    // createLogger(),
-    thunk,
+  composeWithDevTools(
+    applyMiddleware(
+      createLogger(),
+      thunk,
+    ),
   ),
 );
 
